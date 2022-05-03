be97d59750ab75aa8d3c596ecd545ba34df9f8df
Jean-Baptiste DP <jb.durandpierre@gmail.com>
2022-05-03 17:20:40 +0200

    add text input dynamic

--- remplissageGrille.js
+++ remplissageGrille.js
@@ -1,6 +1,39 @@
 let height=5
 let width=5
 
+var listNbInput={
+    hori:[1,1,1,1,1],
+    vert:[1,1,1,1,1]
+}
+
+function modificationInput(orientation,nLigne,nInput){
+    
+    let elements=document.getElementById("config-input-"+orientation+"-"+nLigne+"-"+nInput)
+    
+
+    let value=elements.value;
+    if(nInput===listNbInput[orientation][nLigne-1]){
+        console.log("premiere condition passee")
+        if(value!='0' && value!=''){
+            console.log("deuxieme condition passee")
+            let ligne=document.getElementById(orientation+"-"+nLigne)
+            var input = document.createElement("input")
+            input.id="config-input-"+orientation+"-"+nLigne+"-"+(nInput+1)
+            input.type="number"
+            input.min="0"
+            input.max="99"
+            input.value=""
+            //input.onchange="modificationInput("+orientation+","+nLigne+","+(nInput+1)+")"
+            input.addEventListener('keyup', function(){
+                modificationInput(orientation,nLigne,nInput+1)
+              });
+            input.classList.add("config-input")
+            ligne.appendChild(input)
+            listNbInput[orientation][nLigne-1]+=1
+        }
+    }
+}
+
 function addRow(){
     height+=1
 
@@ -10,7 +43,18 @@ function addRow(){
     //add a column in the config part
     document.getElementById("config_horizontale").style["grid-template-rows"]="repeat("+height+",minmax(20px,1fr))"
     var nvelleDiv = document.createElement("div")
+    nvelleDiv.id = "hori-"+height
     var input = document.createElement("input")
+    input.id="config-input-hori-"+height+"-1"
+    input.type="number"
+    input.min="0"
+    input.max="99"
+    input.value="0"
+    //input.onchange="modificationInput(hori,"+height+",1)"
+    input.addEventListener('keyup', function(){
+        modificationInput("hori",height,1)
+      });
+    input.classList.add("config-input")
     nvelleDiv.appendChild(input)
     document.getElementById("config_horizontale").appendChild(nvelleDiv)
 
@@ -20,6 +64,9 @@ function addRow(){
         var nvelleDiv = document.createElement("div")
         document.getElementById("cases").appendChild(nvelleDiv)
     }
+
+    //dynamic control
+    listNbInput["hori"].push(1)
 }
 
 function addColumn(){
@@ -31,8 +78,17 @@ function addColumn(){
     //add a row in the config part
     document.getElementById("config_verticale").style["grid-template-columns"]="repeat("+width+",minmax(20px,1fr))"
     var nvelleDiv = document.createElement("div")
+    nvelleDiv.id = "vert-"+width
     var input = document.createElement("input")
-    input.id="config-input"
+    input.id="config-input-vert-"+width+"-1"
+    input.type="number"
+    input.min="0"
+    input.max="99"
+    input.value="0"
+    //input.onchange="modificationInput(hori,"+width+",1)"
+    input.addEventListener('keyup', function(){
+        modificationInput("vert",width,1)
+      });
     input.classList.add("config-input")
     nvelleDiv.appendChild(input)
     document.getElementById("config_verticale").appendChild(nvelleDiv)
@@ -43,9 +99,24 @@ function addColumn(){
         var nvelleDiv = document.createElement("div")
         document.getElementById("cases").appendChild(nvelleDiv)
     }
+
+    
+    //dynamic control
+    listNbInput["vert"].push(1)
 }
 
 document.getElementById("plus-horizontal").addEventListener("click",()=>{addRow()})
 document.getElementById("plus-vertical").addEventListener("click",()=>{addColumn()})
 
-console.log("page chargé")
+for(let i=1;i<6;i++){
+    let elehor=document.getElementById("config-input-hori-"+i+"-1")
+    elehor.addEventListener('keyup', function(){
+        modificationInput("hori",i,1)
+    });
+    let elever=document.getElementById("config-input-vert-"+i+"-1")
+    elever.addEventListener('keyup', function(){
+        modificationInput("vert",i,1)
+    });
+}
+
+console.log("page chargée")
